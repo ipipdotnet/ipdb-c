383b64d7113541d50baa4eeccd1b818bb5f09e7b
rain <ssiori@gmail.com>
2018-11-15 11:07:12 +0800

    find

--- README.md
+++ README.md
@@ -4,3 +4,72 @@ IPIP.net officially supported IP database ipdb format parsing library
 see : https://github.com/ipipdotnet/ipdb-cplusplus
 
 gcc -std=gnu99 main.c ipdb.c -ljson-c -o main
+
+
+## Dependence
+```sh
+dnf install json-c-devel
+```
+
+## Build & Run & Output
+```sh
+gcc -std=gnu99 main.c ipdb.c -ljson-c -o main
+
+./main
+
+new ipdb reader err: 0
+ipdb build time: 1535451865
+ipdb ipv4 support: 0
+ipdb ipv6 support: 1
+ipdb language: CN EN 
+...
+```
+
+## Example
+```c
+#include <stdio.h>
+#include <stdlib.h>
+#include "ipdb.h"
+
+int main() {
+    ipdb_reader *reader;
+    int err = ipdb_reader_new("/root/cpp/ipdb-c/mydata6vipday4.ipdb", &reader);
+    printf("new ipdb reader err: %d\n", err);
+    if (!err) {
+        printf("ipdb build time: %li\n", reader->meta->build_time);
+        printf("ipdb ipv4 support: %i\n", ipdb_reader_is_ipv4_support(reader));
+        printf("ipdb ipv6 support: %i\n", ipdb_reader_is_ipv6_support(reader));
+        printf("ipdb language: ");
+        for (int i = 0; i < reader->meta->language_length; ++i) {
+            printf("%s ", reader->meta->language[i].name);
+        }
+        printf("\n");
+        printf("ipdb fields: ");
+        for (int i = 0; i < reader->meta->fields_length; ++i) {
+            printf("%s ", reader->meta->fields[i]);
+        }
+        printf("\n");
+
+        char *lang[2];
+        lang[0] = "CN";
+        lang[1] = "EN";
+        for (int i = 0; i < 2; ++i) {
+            ipdb_string_chain *body;
+            err = ipdb_reader_find(reader, "2001:250:200::", lang[i], &body);
+            printf("%s find err: %d\n", lang[i], err);
+            if (!err) {
+                ipdb_string_chain *temp = body;
+                int j = 0;
+                while (temp) {
+                    printf("%d: %s: %s\n", j + 1, reader->meta->fields[j], temp->str);
+                    ++j;
+                    temp = temp->next;
+                }
+            }
+            ipdb_string_chain_free(&body);
+        }
+    }
+    ipdb_reader_free(&reader);
+    return 0;
+}
+```
--- main.c
+++ main.c
@@ -6,37 +6,39 @@ int main() {
     ipdb_reader *reader;
     int err = ipdb_reader_new("/root/cpp/ipdb-c/mydata6vipday4.ipdb", &reader);
     printf("new ipdb reader err: %d\n", err);
-    printf("ipdb build time: %li\n", reader->meta->build_time);
-    printf("ipdb ipv4 support: %i\n", ipdb_reader_is_ipv4_support(reader));
-    printf("ipdb ipv6 support: %i\n", ipdb_reader_is_ipv6_support(reader));
-    printf("ipdb language: ");
-    for (int i = 0; i < reader->meta->language_length; ++i) {
-        printf("%s ", reader->meta->language[i].name);
-    }
-    printf("\n");
-    printf("ipdb fields: ");
-    for (int i = 0; i < reader->meta->fields_length; ++i) {
-        printf("%s ", reader->meta->fields[i]);
-    }
-    printf("\n");
-
-    char *lang[2];
-    lang[0] = "CN";
-    lang[1] = "EN";
-    for (int i = 0; i < 2; ++i) {
-        ipdb_string_chain *body;
-        err = ipdb_reader_find(reader, "2001:250:200::", lang[i], &body);
-        printf("%s find err: %d\n", lang[i], err);
-        if (!err) {
-            ipdb_string_chain *temp = body;
-            int j = 0;
-            while (temp) {
-                printf("%d: %s: %s\n", j + 1, reader->meta->fields[j], temp->str);
-                ++j;
-                temp = temp->next;
-            }
+    if (!err) {
+        printf("ipdb build time: %li\n", reader->meta->build_time);
+        printf("ipdb ipv4 support: %i\n", ipdb_reader_is_ipv4_support(reader));
+        printf("ipdb ipv6 support: %i\n", ipdb_reader_is_ipv6_support(reader));
+        printf("ipdb language: ");
+        for (int i = 0; i < reader->meta->language_length; ++i) {
+            printf("%s ", reader->meta->language[i].name);
         }
-        ipdb_string_chain_free(&body);
+        printf("\n");
+        printf("ipdb fields: ");
+        for (int i = 0; i < reader->meta->fields_length; ++i) {
+            printf("%s ", reader->meta->fields[i]);
+        }
+        printf("\n");
+
+        char *lang[2];
+        lang[0] = "CN";
+        lang[1] = "EN";
+        for (int i = 0; i < 2; ++i) {
+            ipdb_string_chain *body;
+            err = ipdb_reader_find(reader, "2001:250:200::", lang[i], &body);
+            printf("%s find err: %d\n", lang[i], err);
+            if (!err) {
+                ipdb_string_chain *temp = body;
+                int j = 0;
+                while (temp) {
+                    printf("%d: %s: %s\n", j + 1, reader->meta->fields[j], temp->str);
+                    ++j;
+                    temp = temp->next;
+                }
+            }
+            ipdb_string_chain_free(&body);
+        }
     }
     ipdb_reader_free(&reader);
     return 0;
